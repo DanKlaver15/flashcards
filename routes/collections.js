59faf8705ad23aca4a4e498dce1e577475d9284a
DKlaver <dklaver15@gmail.com>
2021-03-03 16:56:52 -0600

    Altered the import statements in an attempt to get the API to connect with MongoDB.

--- routes/collections.js
+++ routes/collections.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
-const Collection = require('../models/Collection');
+const { Collection } = require('../models/Collection');
 const { Card } = require('../models/Card');
 
 //Collection endpoints
