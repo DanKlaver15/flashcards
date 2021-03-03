a24e589fb088265cc5ebf355e46f4170a9fea9c6
DKlaver <dklaver15@gmail.com>
2021-03-02 18:57:40 -0600

    Began building method for get request.

--- flashcards/src/components/app.jsx
+++ flashcards/src/components/app.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import TitleBar from './TitleBar/titleBar';
+import Carousel from './Carousel/carousel';
 import Footer from './Footer/footer';
 import "./app.css";
 
 class App extends Component { 
-	state={ }
+	constructor(props) {
+		super(props);
+		this.state={ }
+	}
+
+	getAllCards() {
+		axios.get()
+		.then(data =>
+			this.setState({
+				music: data.data,
+				dataReady: true
+			})
+		)
+	}
+
 	render() {
 		return (
 				<div id="main">
 					<TitleBar />
+					<Carousel />
 					<Footer />
 				</div>
 		);
