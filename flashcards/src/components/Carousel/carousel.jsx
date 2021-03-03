1bd9768764680ba9358feb1c538577d628345558
DKlaver <dklaver15@gmail.com>
2021-03-02 18:58:31 -0600

    Removed unnecessary code and added blur to the slides.

--- flashcards/src/components/Carousel/carousel.jsx
+++ flashcards/src/components/Carousel/carousel.jsx
@@ -6,36 +6,26 @@ const slides = [
     title: "Machu Picchu",
     subtitle: "Peru",
     description: "Adventure is never far away",
-    image:
-      "https://images.unsplash.com/photo-1571771019784-3ff35f4f4277?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
   },
   {
     title: "Chamonix",
     subtitle: "France",
     description: "Let your dreams come true",
-    image:
-      "https://images.unsplash.com/photo-1581836499506-4a660b39478a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
   },
   {
     title: "Mimisa Rocks",
     subtitle: "Australia",
     description: "A piece of heaven",
-    image:
-      "https://images.unsplash.com/photo-1566522650166-bd8b3e3a2b4b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
   },
   {
     title: "Four",
     subtitle: "Australia",
     description: "A piece of heaven",
-    image:
-      "https://images.unsplash.com/flagged/photo-1564918031455-72f4e35ba7a6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
   },
   {
     title: "Five",
     subtitle: "Australia",
     description: "A piece of heaven",
-    image:
-      "https://images.unsplash.com/photo-1579130781921-76e18892b57b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
   }
 ];
 
@@ -113,15 +103,12 @@ function Slide({ slide, offset }) {
       style={{
         "--offset": offset,
         "--dir": offset === 0 ? 0 : offset > 0 ? 1.5 : -1.5,
-		  blur(5px)
+		  "--blur": offset !== 0 ? 1 : 0,
+		  "--lower": offset !== 0 ? 1 : 0,		// background slides are lower on screen
+		  "--higher": offset === 0 ? 1 : 0,		//center slide is higher and in focus
       }}
     >
-      <div
-        className="slideContent"
-        style={{
-          backgroundImage: `url('${slide.image}')`
-        }}
-      >
+      <div className="slideContent">
         <div className="slideContentInner">
           <h2 className="slideTitle">{slide.title}</h2>
           <h3 className="slideSubtitle">{slide.subtitle}</h3>
