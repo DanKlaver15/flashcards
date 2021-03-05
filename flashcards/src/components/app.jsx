37bb6e479cea39d70e0d06020576d3d858e962bd
DKlaver <dklaver15@gmail.com>
2021-03-04 20:59:06 -0600

    Rewrote component using hooks instead of classes and finalized code for API call to MongoDB to get all cards for a specific collection of flashcards.

--- flashcards/src/components/app.jsx
+++ flashcards/src/components/app.jsx
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TitleBar from './TitleBar/titleBar';
-import Carousel from './Carousel/carousel';
+import CardViewer from './CardViewer/cardViewer';
 import Footer from './Footer/footer';
 import "./app.css";
 
-class App extends Component { 
-	constructor(props) {
-		super(props);
-		this.state={ }
+function App() {
+	const [cardData, setCardData] = useState([]);
+	const [dataReady, setDataReady] = useState(false);
+
+	let temp = "603e7562a7b8813da19a7d40";
+	function getCardsFromCollection(collectionID) {
+		axios.get(`http://localhost:5000/api/collections/${collectionID}/cards`)
+		.then(data =>	setCardData(data.data), setDataReady(true));
 	}
 
-	getAllCards() {
-		axios.get()
-		.then(data =>
-			this.setState({
-				music: data.data,
-				dataReady: true
-			})
-		)
-	}
+	useEffect(() => {
+		getCardsFromCollection(temp);
+	}, [temp]);
 
-	render() {
-		return (
-				<div id="main">
-					<TitleBar />
-					<Carousel />
-					<Footer />
-				</div>
-		);
-	}
+	return (
+		dataReady ?
+			<div id="main">
+				<TitleBar />
+				<CardViewer cardData={cardData} />
+				<Footer />
+			</div>
+		: null
+	);
 }
 
 export default App;
