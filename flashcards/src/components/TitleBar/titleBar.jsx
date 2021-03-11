0e7ab7d2c062afe559cb513502b04b769e42dcdd
DKlaver <dklaver15@gmail.com>
2021-03-11 16:03:29 -0600

    Imported the edit collections API call from app.jsx.

--- flashcards/src/components/TitleBar/titleBar.jsx
+++ flashcards/src/components/TitleBar/titleBar.jsx
@@ -8,7 +8,7 @@ function TitleBar(props) {
    return (
 		<div className="titlebar">
 			<div className='menuButton'>
-			<TemporaryDrawer allData={props.allData} selectCollection={props.selectCollection} addCollection={props.addCollection} deleteCollection={props.deleteCollection}/>
+			<TemporaryDrawer allData={props.allData} selectCollection={props.selectCollection} addCollection={props.addCollection} editCollection={props.editCollection} deleteCollection={props.deleteCollection}/>
 			</div>
 			<SetCurrentTitle />
 		</div>
