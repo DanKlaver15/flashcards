4f85f40b78d35600c5fb9cb64daf9984ce1b30f6
DKlaver <dklaver15@gmail.com>
2021-03-14 19:21:24 -0500

    Added an accent color bar.

--- flashcards/src/components/TitleBar/titleBar.jsx
+++ flashcards/src/components/TitleBar/titleBar.jsx
@@ -7,8 +7,19 @@ function TitleBar(props) {
 
    return (
 		<div className="titlebar">
+			<div className="accentBarTop"></div>
 			<div className='menuButton'>
-			<TemporaryDrawer allData={props.allData} selectCollection={props.selectCollection} addCollection={props.addCollection} editCollection={props.editCollection} deleteCollection={props.deleteCollection}/>
+			<TemporaryDrawer 
+				allData={props.allData}
+				collectionData={props.collectionData}
+				selectCollection={props.selectCollection} 
+				addCollection={props.addCollection} 
+				editCollection={props.editCollection} 
+				deleteCollection={props.deleteCollection}
+				addCard={props.addCard}
+				editCard={props.editCard}
+				deleteCard={props.deleteCard}
+			/>
 			</div>
 			<SetCurrentTitle />
 		</div>
