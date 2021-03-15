652bd8dd98f918b1845fd6dc7932066b188e0352
DKlaver <dklaver15@gmail.com>
2021-03-15 13:58:10 -0500

    Added additional API calls.

--- flashcards/src/components/app.jsx
+++ flashcards/src/components/app.jsx
@@ -20,12 +20,13 @@ function App() {
 	function selectCollection(collectionID) {
 		setCollectionData([]);
 		axios.get(`http://localhost:5000/api/collections/${collectionID}/cards`)
-		.then(data =>	setCollectionData(data.data), setDataReady(true));
+		.then(data =>	setCollectionData(data.data), setDataReady(true))
+		.then(() => getAllCards(connectionURI));
 	}
 
 	function addCollection(title) {
 		axios.post(connectionURI, title)
-		.then(setDataReady(false))
+		.then(setDataReady(true))
 		.then(() => getAllCards(connectionURI));
 	}
 
@@ -41,6 +42,24 @@ function App() {
 		.then(() => getAllCards(connectionURI));
 	}
 
+	function addCard(collectionID, cardInfo) {
+		axios.post(`http://localhost:5000/api/collections/${collectionID}/cards`, cardInfo)
+		.then(setDataReady(true))
+		.then(() => getAllCards(connectionURI));
+	}
+
+	function editCard(collectionID, cardID, cardInfo) {
+		axios.put(`http://localhost:5000/api/collections/${collectionID}/cards/${cardID}`, cardInfo)
+		.then(setDataReady(true))
+		.then(() => getAllCards(connectionURI));
+	}
+	
+	function deleteCard(collectionID, cardID) {
+		axios.delete(`http://localhost:5000/api/collections/${collectionID}/cards/${cardID}`)
+		.then(setDataReady(false))
+		.then(() => getAllCards(connectionURI));
+	}
+
 	useEffect(() => {
 		getAllCards(connectionURI);
 	}, [connectionURI]);
@@ -48,7 +67,17 @@ function App() {
 	return (
 		dataReady ?
 			<div id="main">
-				<TitleBar allData={allData} selectCollection={selectCollection} collectionData={collectionData} addCollection={addCollection} editCollection={editCollection} deleteCollection={deleteCollection}/>
+				<TitleBar 
+					allData={allData} 
+					selectCollection={selectCollection} 
+					collectionData={collectionData} 
+					addCollection={addCollection} 
+					editCollection={editCollection} 
+					deleteCollection={deleteCollection}
+					addCard={addCard}
+					editCard={editCard}
+					deleteCard={deleteCard}
+				/>
 				<CardViewer collectionData={collectionData} />
 				<Footer />
 			</div>
