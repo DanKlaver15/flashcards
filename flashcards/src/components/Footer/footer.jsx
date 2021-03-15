c5ca9163a21dcef20a30e52b3db4231be96f29db
DKlaver <dklaver15@gmail.com>
2021-03-15 13:57:34 -0500

    Removed slide counter and moved it to the bottom of each slide.

--- flashcards/src/components/Footer/footer.jsx
+++ flashcards/src/components/Footer/footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-// import { SetSlideCount } from '../CardViewer/cardViewer';
 import './footer.css';
 
 function Footer(props) { 
    return (
 		<div className="footer">
 			<div className='accentBar'></div>
-			{/* <SetSlideCount /> */}
 		</div>
 	);
 }
